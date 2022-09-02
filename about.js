10208999dade8ee51b999222979e234b5d49cc88
Kelsey Taylor <kelseytay94@gmail.com>
2022-09-02 16:27:57 -0600

    post assessment

--- about.js
+++ about.js
@@ -1,20 +1,26 @@
 console.log("hello world");
 
+//Step 1: Select an element
+let form = document.querySelector("#contact");
+let duckForm = document.querySelector("#rubberDuck");
+let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+
+//Step 2: Write the function
 function handleSubmit(evt) {
 	evt.preventDefault();
 
 	alert("Form has been submitted!");
 	// console.log('form submit');
+	input1.value = "";
+	input2.value = "";
 }
-
-let form = document.querySelector("#contact");
-
-form.addEventListener("submit", handleSubmit);
 
 const complimentDuck = () => {
 	alert("Quack! You are a beautiful human!");
 };
 
-let duckForm = document.querySelector("#rubberDuck");
+//Step 3: Combine them with event listener
+form.addEventListener("submit", handleSubmit);
 
 duckForm.addEventListener("mouseover", complimentDuck);
--- profile.js
+++ profile.js
@@ -1,27 +1,32 @@
-// favorite color button
+//Step 1: Select Element
+let favoriteColor = document.getElementById("color");
+let favoritePlace = document.getElementById("place");
+let favoriteRitual = document.getElementById("ritual");
+let movieForm = document.querySelector("form");
 
+//Step 2: Write Functions
 const favColorBtn = () => {
 	alert("My favorite color is teal-green!");
 };
 
-let favoriteColor = document.getElementById("color");
-
-favoriteColor.addEventListener("click", favColorBtn);
-
-//favorite place button
 const favPlaceBtn = () => {
 	alert("My favorite Place is up in the mountains. Big Cottonwood Canyon.");
 };
 
-let favoritePlace = document.getElementById("place");
-
-favoritePlace.addEventListener("click", favPlaceBtn);
-
-//favorite ritual button
 const favRitualBtn = () => {
 	alert("My favorite ritual is bedtime with my baby: Bathtime, snuggle up cozy, feed her, and sing songs to sleep.");
 };
 
-let favoriteRitual = document.getElementById("ritual");
+const submitForm = (event) => {
+	event.preventDefault();
+
+	alert("Incorrect! They are ALL the best LOTR movie.");
+};
+
+//Step 3: Combine 1 & 2 with Event Listener
+favoriteColor.addEventListener("click", favColorBtn);
+
+favoritePlace.addEventListener("click", favPlaceBtn);
 
 favoriteRitual.addEventListener("click", favRitualBtn);
+movieForm.addEventListener("submit", submitForm);
